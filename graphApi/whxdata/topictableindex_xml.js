963858e7e4174ed6e0450e7c03ce8ad413886f32
Michael Connor <mconnor@mindjet.com>
2015-05-04 12:20:50 -0700

    initial commit

---
+++ graphApi/whxdata/topictableindex_xml.js
@@ -0,0 +1,1 @@
+gXMLBuffer="<?xml version=\"1.0\" encoding=\"UTF-8\"?><cki><ck fm=\"Acceptance Class\" to=\"FormData Class\" num=\"67\"/><ck fm=\"FormField Class\" to=\"RDF (Semantic Web) Concepts\" num=\"76\"/><ck fm=\"Reference\" to=\"XML-Based Datatypes\" num=\"40\"/></cki>";
